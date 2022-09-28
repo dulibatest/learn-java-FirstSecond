2dc55f99c3d3ade80e417066c61f02a0d101747b
dulibatest <duliba@qarea.us>
2022-09-28 23:58:38 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char appleSort1 = 'F';
+        char appleSort2 = 'S';
+        boolean is = true;
+
+        if (appleSort1 == 'S' || appleSort2 == 'T')
+            is = true;
+        else
+            is = false;
+        System.out.println(is);
+
+        }
+
+
+    }
+
