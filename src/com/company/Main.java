d93af8c0a62116d6d2ba67298c4c51627cc1132f
dulibatest <duliba@qarea.us>
2022-10-07 21:29:48 +0300

    Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,75 +8,162 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //Task 4
-       float meters = 5.0f;
-       float inches = meters * 39.37f;
-        System.out.println("Inches: " + inches);
-        System.out.println("Meters: " + inches/39.37f);
-        System.out.println(" ");
 
-        //Task 5
-        float kilometres = 6.0f;
-        double miles = 1.609 * kilometres;
-        System.out.println("Miles: " + miles);
-        System.out.println("Kilometres: " + miles / 1.609);
-        System.out.println(" ");
+    //Task 1
+        /*int mark = 40;
+        if (mark >= 50){
+            System.out.println("PASS");
+        }
+        else {
+            System.out.println("FAIL");
+        }
+        System.out.println("Done"); */
+    //Task 2
+       /* int number = 7;
+        if ( number % 2 != 0) {
+            System.out.println("Odd number");
+        }
+        else {
+            System.out.println("Even number");
+        }
+        System.out.println("Done"); */
+        //Task 3
+        /*int number = 5;
+        if(number == 1){
+            System.out.println("One");
+        }
+        else if (number == 2){
+            System.out.println("Two");
+        }
+        else if (number ==3){
+            System.out.println("Three");
+        }
+        else if (number==4){
+            System.out.println("Four");
+        }
+        else if (number==5){
+            System.out.println("Five");
+        }
 
-        //Task 6
-        double kilograms = 7;
-        double pounds = kilograms * 2.2046;
-        System.out.println("Pounds: " + pounds);
-        System.out.println("Kilograms: " + pounds / 2.2046);
-        System.out.println(" ");
+        int number = 9;
+        switch (number) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+        } */
+    //Task 4
+       /* int number = 8;
+        if(number==1){
+            System.out.println("Monday");
+        }
+        else if (number==2){
+            System.out.println("Tuesday");
+        }
+        else if (number==3){
+            System.out.println("Wednesday");
+        }
+        else if (number==4){
+            System.out.println("Thursday");
+        }
+        else if (number==5){
+            System.out.println("Friday");
+        }
+        else if (number==6){
+            System.out.println("Saturday");
+        }
+        else if (number==7){
+            System.out.println("Sunday");
+        }
+        else {
+            System.out.println("Not a valid day");
+        } */
 
-        //Task 7
-        double KmHr = 60;
-        double MiHr = KmHr * 0.6214;
-        System.out.println("Miles for hour: " + MiHr);
-        System.out.println("Kilimetrs fot hour: " + MiHr / 0.6214);
-        System.out.println(" ");
+        /*int number = 9;
+        switch (number) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Not a valid day");
+            }*/
+    //Task 5
+        /*int x = 5;
+        int y = 8;
+        if (x > y){
+            System.out.println(x + " is bigger than " + y);}
+        else if (y > x){
+            System.out.println(y + " is bigger than " + x);}
+        else {
+            System.out.println("These numbers are equal");} */
 
-        //Task 8
-        int test = 10;
-        int mark = 8;
-        int result = mark * 100 / test;
-        System.out.println("Result: " + result + "%");
-        System.out.println(" ");
 
-        //Task 9
-        int ageHusband = 46;
-        int ageWife = 45;
-        boolean age = true;
-        if (ageHusband < 35 && ageWife < 35)
-            age = true;
-        else age = false;
-        System.out.println(age);
-        System.out.println(" ");
+    //Task 6
+        /*int a = 5;
+        int b = 7;
+        int x; */
 
-        //Task 10
-        int income = 500000;
-        boolean familyIncome = true;
-        if (income > 20000 & income < 50000)
-            familyIncome = true;
-        else
-            familyIncome = false;
-        System.out.println(familyIncome);
-        System.out.println(" ");
+    //Task 7
+        /*int stavka = 1000;
+        int prodagi = 12;
+        int salary;
 
-        //Task 11
-        char appleSort1 = 'F';
-        char appleSort2 = 'S';
-        boolean is = true;
-        if (appleSort1 == 'S' || appleSort2 == 'T')
-            is = true;
-        else
-            is = false;
-        System.out.println(is);
-        System.out.println(" ");
+        if (prodagi > 10){
+            salary = stavka + 250;
+            System.out.println(salary);
+        }*/
+
+    //Task 8
+        /*int QuantitySell = 7;
+        int MinimumToBonus = 11;
+        int QuantityLeft;
+        if (QuantitySell >= MinimumToBonus) {
+            System.out.println("You will have a bonus");
+        }
+        if (QuantitySell < MinimumToBonus){
+            QuantityLeft = MinimumToBonus - QuantitySell;
+            System.out.println(QuantityLeft);
+        }*/
+
+    //Task 9
+
 
 
         }
 
 
-    }
+
+        }
+
+
+
 
